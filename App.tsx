cdc6e1e65556ccf030a275df29dd5eac534109d0
Anh Duy <duynguyen1bb@gmail.com>
2025-02-07 21:15:46 +0700

    First commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import Home from './src/screens/Home';
+import Login from './src/screens/Login';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Login' screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='Login' component={Login} />
+        <Stack.Screen name='Home' component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App
